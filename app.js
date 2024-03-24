27759c3a4672aa841172828a5b084309625e786c
jorge <stevenhijo@hotmail.com>
2024-03-24 14:00:19 -0500

    CORECCION CARPETA

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ function playAudio(btn) {
   audio.currentTime = 0;
   audio.play();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const p = document.querySelector('.letras p');
+  const cursor = document.createElement('span');
+  cursor.classList.add('cursor');
+  p.appendChild(cursor);
+});
+
+function openVideo() {
+  document.getElementById("video-overlay").style.display = "block";
+}
+
+function closeVideo() {
+  document.getElementById("video-overlay").style.display = "none";
+  var video = document.getElementById("video-iframe");
+  video.src = video.src; // Detener el video al cerrar la caja
+}
